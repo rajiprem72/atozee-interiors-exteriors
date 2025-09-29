529072017f19ec1ccf18d8bff3902deebdb65e50
Premanandhan Narayanan <premanandhan@gmail.com>
2025-09-29 19:19:54 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Placeholder for future features (animations, interactivity)
+// Currently just logs page load
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("A TO ZEE INTERIOR AND EXTERIORS - Page Loaded");
+});
